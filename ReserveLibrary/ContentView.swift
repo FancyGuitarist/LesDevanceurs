0aa20d6bdcb86d37431604907a236c4f23a75747
FancyGuitarist <simf3010@gmail.com>
2023-01-30 13:04:01 -0500

    The App Is Ready
    Les Devanceurs devancent

--- ReserveLibrary/ContentView.swift
+++ ReserveLibrary/ContentView.swift
@@ -8,19 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("Devanceurs")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Text("Bibliothèque")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Link(destination: URL(string: "https://bibl-ulaval.libcal.com/space/12227")!) {
+                    Text("Réservation")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .frame(width: 250, height: 50)
+                        .background(RoundedRectangle(cornerRadius: 20).fill(Color.white).shadow(radius: 3))}
+                    Link(destination: URL(string: "https://bibl-ulaval.libcal.com/r/checkin/kXP42iOk")!) {Text("Confirmation")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .frame(width: 250, height: 50)
+                            .background(RoundedRectangle(cornerRadius: 20).fill(Color.white).shadow(radius: 3))
+                    }
+                    Spacer()
+                Text("Overleaf")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                
+                Link(destination: URL(string: "https://www.overleaf.com/6934683944nsncrscprdbj")!) {
+                    Text("Matériaux")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .frame(width: 250, height: 50)
+                    .background(RoundedRectangle(cornerRadius: 20).fill(Color.white).shadow(radius: 3))}
+                    Link(destination: URL(string: "https://fr.overleaf.com/1223156833dvrqgcxzhcxp")!) {
+                        Text("Quantique")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .fixedSize(horizontal: false, vertical: true)
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .frame(width: 250, height: 50)
+                                .background(RoundedRectangle(cornerRadius: 20).fill(Color.white).shadow(radius: 3))
+                    }
+                    Spacer()
+                    
+                }
+            }
+            
         }
-        .padding()
     }
-}
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
