5922d26d206914105570880e910388cb28cc4933
FancyGuitarist <simf3010@gmail.com>
2023-01-30 11:36:36 -0500

    Initial Commit

---
+++ ReserveLibrary/ReserveLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReserveLibraryApp.swift
+//  ReserveLibrary
+//
+//  Created by Simon Ferland on 2023-01-30.
+//
+
+import SwiftUI
+
+@main
+struct ReserveLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
