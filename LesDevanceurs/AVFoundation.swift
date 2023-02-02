a365907306620e9ed06e53a34d9ebdf800e45bdf
FancyGuitarist <simf3010@gmail.com>
2023-02-02 12:09:41 -0500

    Forgot to stage changes
    r/oof

--- LesDevanceurs/AVFoundation.swift
+++ LesDevanceurs/AVFoundation.swift
@@ -5,4 +5,19 @@
 //  Created by Simon Ferland on 2023-02-02.
 //
 
-import Foundation
+import AVFoundation
+
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: "AvengersTest", ofType: "mp3") {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+            print("It Works")
+        } catch {
+            print("ERROR")
+        }
+    }
+}
--- LesDevanceurs/ContentView.swift
+++ LesDevanceurs/ContentView.swift
@@ -6,22 +6,32 @@
 //
 
 import SwiftUI
+import AVFoundation
+
+
+
 
 struct ContentView: View {
+    @State var audioPlayer: AVAudioPlayer!
     
     var body: some View {
+        
         ZStack {
             Image("Devanceurs")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .ignoresSafeArea()
+                .onAppear(perform: {
+                    playSounds("AvengersTest.mp3")
+                })
+            
             VStack{
                 Spacer()
                 Text("Bibliothèque")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .foregroundColor(Color.white)
-                Link(destination: URL(string: "https://bibl-ulaval.libcal.com/space/12227")!) {
+                Link(destination: URL(string: "https://bibl-ulaval.libcal.com/spaces?lid=1809")!) {
                     Text("Réservation")
                         .font(.largeTitle)
                         .fontWeight(.bold)
@@ -29,17 +39,17 @@ struct ContentView: View {
                         .multilineTextAlignment(.center)
                         .padding()
                         .frame(width: 250, height: 50)
-                        .background(RoundedRectangle(cornerRadius: 20).fill(Color.white).shadow(radius: 3))}
-                    Link(destination: URL(string: "https://bibl-ulaval.libcal.com/r/checkin/kXP42iOk")!) {Text("Confirmation")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .fixedSize(horizontal: false, vertical: true)
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .frame(width: 250, height: 50)
-                            .background(RoundedRectangle(cornerRadius: 20).fill(Color.white).shadow(radius: 3))
-                    }
-                    Spacer()
+                    .background(RoundedRectangle(cornerRadius: 20).fill(Color.white).shadow(radius: 3))}
+                Link(destination: URL(string: "https://bibl-ulaval.libcal.com/r/checkin")!) {Text("Confirmation")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .frame(width: 250, height: 50)
+                        .background(RoundedRectangle(cornerRadius: 20).fill(Color.white).shadow(radius: 3))
+                }
+                Spacer()
                 Text("Overleaf")
                     .font(.largeTitle)
                     .fontWeight(.bold)
@@ -54,28 +64,41 @@ struct ContentView: View {
                         .padding()
                         .frame(width: 250, height: 50)
                     .background(RoundedRectangle(cornerRadius: 20).fill(Color.white).shadow(radius: 3))}
-                    Link(destination: URL(string: "https://fr.overleaf.com/1223156833dvrqgcxzhcxp")!) {
-                        Text("Quantique")
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .fixedSize(horizontal: false, vertical: true)
-                                .multilineTextAlignment(.center)
-                                .padding()
-                                .frame(width: 250, height: 50)
-                                .background(RoundedRectangle(cornerRadius: 20).fill(Color.white).shadow(radius: 3))
-                    }
-                    Spacer()
-                    
+                Link(destination: URL(string: "https://fr.overleaf.com/1223156833dvrqgcxzhcxp")!) {
+                    Text("Quantique")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .frame(width: 250, height: 50)
+                        .background(RoundedRectangle(cornerRadius: 20).fill(Color.white).shadow(radius: 3))
                 }
+                Spacer()
+                
             }
-            
         }
     }
-    
+    func playSounds(_ soundFileName : String) {
+            guard let soundURL = Bundle.main.url(forResource: "AvengersTest", withExtension: "mp3") else {
+                fatalError("Unable to find \(soundFileName) in bundle")
+            }
+
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            } catch {
+                print(error.localizedDescription)
+            }
+            audioPlayer.play()
+        print("This works")
+        }
+}
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
         }
     }
+    
+    
     
 
